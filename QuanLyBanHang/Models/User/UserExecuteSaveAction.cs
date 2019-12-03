4b050ae3a15c1ca83ad2d9ee8be97c7498a4b2d3
TranBaKhoa1999 <ohwhynotme1999@gmail.com>
2019-12-03 15:51:44 +0700

    xong User

---
+++ QuanLyBanHang/Models/User/UserExecuteSaveAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserBussiness;
+
+namespace QuanLyBanHang.Models
+{
+    public class UserExecuteSaveAction
+    {
+        public string EditFlag { get; set; }
+        public Domain.User UserList { get; set; }
+        public bool Execute()
+        {
+            using (var cmd = new UserExecuteSaveBussiness())
+            {
+                cmd.EditFlag = this.EditFlag;
+                cmd.Item = this.UserList;
+                return cmd.Execute();
+            }
+        }
+    }
+}
---
+++ UserRepository/UserUpdateRepository.cs
@@ -0,0 +1,23 @@
+﻿using ConnectDataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UserUpdateRepository :Connection
+    {
+        public Domain.User Item { get; set; }
+        public bool Execute()
+        {
+            using (var cmd = new Query())
+            {
+                //cmd.QueryString = "UPDATE [dbo].[User] SET [Password] = '" + Item.Password+ "' ,[Email] = '" + Item.Email + "' WHERE [User].Username = '" + Item.Username + "'";
+                cmd.QueryString = "UPDATE [dbo].[User] SET [Password]='" + Item.Password + "' WHERE [User].Username='"+Item.Username+"'";
+                return cmd.ExecuteQueryNonReader();
+            }
+        }
+    }
+}
