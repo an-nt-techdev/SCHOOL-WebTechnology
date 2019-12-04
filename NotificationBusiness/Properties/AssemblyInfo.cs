761d85a3e1bb6146f5f5ff2af6202523be61b124
Nguyễn Thiên Ân <ndsg1964@gmail.com>
2019-12-04 14:42:57 +0700

    sasas

--- NotificationBusiness/Properties/AssemblyInfo.cs
+++ NotificationBusiness/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotificationExcuteSaveBusiness")]
+[assembly: AssemblyTitle("NotificationBusiness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NotificationExcuteSaveBusiness")]
+[assembly: AssemblyProduct("NotificationBusiness")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a5bb0dc4-2a12-424d-9bec-356209a438c7")]
+[assembly: Guid("644a1650-a071-47f8-b618-3a51cb9a50e1")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ NotificationRepository/NotificationSearchRepository.cs
@@ -0,0 +1,21 @@
+﻿using ConnectDataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    class NotificationSearchRepository : Connection
+    {
+        public List<dynamic> Execute()
+        {
+            using(var cmd = new Query())
+            {
+                cmd.QueryString = "SELECT [dbo][Notification].*, [dbo][Sale].* FROM [dbo][Notification] LEFT JOIN [dbo][Sale] on [dbo][Sale].SaleId = [dbo][Notification].SaleId";
+                return cmd.ExecuteQuery();
+            }
+        }
+    }
+}
